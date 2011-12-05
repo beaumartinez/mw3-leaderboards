f63562650fe4ca8a71b1b23097b98c78b4c18001
Beau <beau@beaumartinez.com>
2011-12-05 20:42:36 +0000

    Parse leaderboard entry attributes

--- mw3-leaderboards.py
+++ mw3-leaderboards.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 import pyquery
 import requests
@@ -67,10 +68,94 @@ def _parse_domination_leaderboard_entry(element):
     kills = kills_element.text_content()
     games_played = games_played_element.text_content()
 
+    rank = rank.replace(',', '')
+    rank = int(rank)
+
+    score = _parse_score(score)
+
+    captures = captures.replace(',', '')
+    captures = int(captures)
+
+    time_played = _parse_time(time_played)
+
+    defends = defends.replace(',', '')
+    defends = int(defends)
+
+    kills = kills.replace(',', '')
+    kills = int(kills)
+
+    games_played = games_played.replace(',', '')
+    games_played = int(games_played)
+
     entry = DominationLeaderboardEntry(rank, name, score, time_played,
         captures, defends, kills, games_played)
 
     return entry
+
+def _parse_score(score):
+    score = score.replace(',', '')
+
+    try:
+        score = int(score)
+    except ValueError:
+        score = re.search('(\d+\.?\d*)MM', score)
+        score = score.group(1)
+
+        score = float(score)
+        score *= 1000000
+
+        score = int(score)
+
+    return score
+
+def _parse_time(time):
+    weeks = re.search('(\d+)w', time)
+    days = re.search('(\d+)d', time)
+    hours = re.search('(\d+)h', time)
+    minutes = re.search('(\d+)m', time)
+    seconds = re.search('(\d+)s', time)
+
+    try:
+        weeks = weeks.group(1)
+    except AttributeError:
+        weeks = 0
+    else:
+        weeks = int(weeks)
+
+    try:
+        days = days.group(1)
+    except AttributeError:
+        days = 0
+    else:
+        days = int(days)
+
+    try:
+        hours = hours.group(1)
+    except AttributeError:
+        hours = 0
+    else:
+        hours = int(hours)
+
+    try:
+        minutes = minutes.group(1)
+    except AttributeError:
+        minutes = 0
+    else:
+        minutes = int(minutes)
+
+    try:
+        seconds = seconds.group(1)
+    except AttributeError:
+        seconds = 0
+    else:
+        seconds = int(seconds)
+
+    seconds += (minutes * 60)
+    seconds += (hours * 360)
+    seconds += (days * 8640)
+    seconds += (weeks * 60480)
+
+    return seconds
 
 class DominationLeaderboardEntry(object):
 
