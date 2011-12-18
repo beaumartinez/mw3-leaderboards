10a13b502c9e9c74b0030ad4fb4fb18b87793d7e
Beau <beau@beaumartinez.com>
2011-12-18 21:11:26 +0000

    Add main module
    python -m likes it that way

---
+++ mw3leaderboards/__main__.py
@@ -0,0 +1,11 @@
+import api
+
+email = raw_input('Email: ')
+password = raw_input('Password: ')
+
+api = api.Api(email, password)
+
+domination_leaderboard = api.get_domination_leaderboard()
+
+for entry in domination_leaderboard.entries:
+    print entry.to_json()
