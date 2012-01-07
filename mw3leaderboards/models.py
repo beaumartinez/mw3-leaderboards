bbfe0719c5f85460eaadfe9d8d1ef827a42c2900
Beau <beau@beaumartinez.com>
2012-01-07 13:32:43 +0000

    Abstract domination leaderboard entry parsing

--- mw3leaderboards/models.py
+++ mw3leaderboards/models.py
@@ -24,33 +24,8 @@ class DominationLeaderboardEntry(LeaderboardEntry):
 
     @classmethod
     def from_element(cls, element):
-        rank_element = element[0]
-        rank = rank_element.text_content()
-        rank = parser.parse_formatted_number(rank)
-
-        name_element = element[1]
-        name = name_element.text_content()
-        name = name.strip()
-
-        score_element = element[2]
-        score = score_element.text_content()
-        score = parser.parse_score(score)
-
-        captures_element = element[5]
-        captures = captures_element.text_content()
-        captures = parser.parse_formatted_number(captures)
-
-        defends_element = element[6]
-        defends = defends_element.text_content()
-        defends = parser.parse_formatted_number(defends)
-
-        kills_element = element[7]
-        kills = kills_element.text_content()
-        kills = parser.parse_formatted_number(kills)
-
-        games_played_element = element[8]
-        games_played = games_played_element.text_content()
-        games_played = parser.parse_formatted_number(games_played)
+        (rank, name, score, captures, defends, kills,
+            games_played) = parser.parse_domination_leaderboard_entry(element)
 
         entry = cls(rank, name, score, captures, defends, kills, games_played)
 
--- mw3leaderboards/parser.py
+++ mw3leaderboards/parser.py
@@ -19,3 +19,34 @@ def parse_formatted_number(number):
     number = int(number)
 
     return number
+
+def parse_domination_leaderboard_entry(element):
+    rank_element = element[0]
+    rank = rank_element.text_content()
+    rank = parse_formatted_number(rank)
+
+    name_element = element[1]
+    name = name_element.text_content()
+    name = name.strip()
+
+    score_element = element[2]
+    score = score_element.text_content()
+    score = parse_score(score)
+
+    captures_element = element[5]
+    captures = captures_element.text_content()
+    captures = parse_formatted_number(captures)
+
+    defends_element = element[6]
+    defends = defends_element.text_content()
+    defends = parse_formatted_number(defends)
+
+    kills_element = element[7]
+    kills = kills_element.text_content()
+    kills = parse_formatted_number(kills)
+
+    games_played_element = element[8]
+    games_played = games_played_element.text_content()
+    games_played = parse_formatted_number(games_played)
+
+    return rank, name, score, captures, defends, kills, games_played
